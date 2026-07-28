4ff3571e74b7bd58d16dfd43aebcd22d561740ac
“rudra273” <rudramohanty45@gmail.com>
2026-07-29 00:53:06 +0530

    4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.daykit"
         minSdk = 31
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
